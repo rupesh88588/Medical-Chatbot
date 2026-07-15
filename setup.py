01c16b1d0bbb2860556fdb1f70c295c4eeecfe92
Rupesh-88588 <172488383+rupesh88588@users.noreply.github.com>
2026-07-15 13:22:48 +0530

    done

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages,setup
+
+setup(
+    name='medical_chatbot',
+    version='1.0.0',
+    author='babu',
+    author_email='babu@outlook.com',
+    packages=find_packages(),
+    install_requires=[]
+)
+
